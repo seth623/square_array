925bab82fbcb9e2d339be8fe1c71d567b642088e
seth <seth623@gmail.com>
2021-12-19 22:06:20 -0700

    Completed Assignment

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  new_array = Array.new 
+  
+  array.each do |number|
+    new_number = number**2
+    new_array.push(new_number)
+  end 
+
+  return new_array
+
+end
+
+def collect_array(array)
+  new_array = array.collect{ |number| number**2 }
 end
